d315c05a6bce373d63348fc6426f6af4df17f65a
OmRaN <OmRaN>
2016-06-17 17:56:52 +0300

    first version

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var dateFormat = require('dateformat');
+var app = express();
+var obj;
+app.all("/", function(req, res){
+    res.sendFile("index.html");
+});
+app.get("/:text", function(req, res) {
+    var date;
+    if (isNaN(parseFloat(req.params.text))) {
+        date = new Date(req.params.text);
+    } else {
+        date = new Date(parseInt(req.params.text) * 1000);
+    }
+    var formatted;
+    if (isNaN(date.getTime())) {
+        formatted = null;
+    } else {
+        formatted = dateFormat(date, "mmmm dd, yyyy");
+    }
+    obj = {
+        unix: date.getTime(),
+        natural: formatted
+    }
+    res.end(JSON.stringify(obj));
+});
+
+app.listen(8000);
